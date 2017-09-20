749b7e09d523d9b3a7f320372691ca464964d8f8
Misbahul Ardani <misbahulard@gmail.com>
2017-09-20 15:32:49 +0700

    My awesome commit

---
+++ app/src/main/java/com/example/misbahulard/bottomnavigationexample/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.misbahulard.bottomnavigationexample;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_lingkaran:
+                    // set fragment
+                    return true;
+                case R.id.navigation_persegi:
+                    // set fragment
+                    return true;
+                case R.id.navigation_segitiga:
+                    // set fragment
+                    return true;
+            }
+
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+    }
+}
